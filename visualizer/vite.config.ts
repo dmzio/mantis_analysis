4e00741fdf74f9c50dfa9e977b8100da1038ad6e
Dmitry <ziodmitry+gh@gmail.com>
2025-07-06 21:52:00 +0200

    fix vue template rendering (#9)

--- visualizer/vite.config.ts
+++ visualizer/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'vue': resolve(__dirname, 'node_modules/vue/dist/vue.esm-bundler.js')
+    }
+  },
   build: {
     outDir: 'dist'
   }
