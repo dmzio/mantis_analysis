666d38fbcbdc6f94eef0deab4c4374a17a56f186
Dmitry <ziodmitry+gh@gmail.com>
2025-07-10 10:26:39 +0200

    chore: fix PrimeVue theming and cleanup docs

--- visualizer/src/main.ts
+++ visualizer/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
+import Lara from '@primevue/themes/lara';
+import 'primeicons/primeicons.css';
 import router from './router';
 import AppRoot from './App';
 import './main.css';
@@ -7,6 +9,7 @@ import './main.css';
 const app = createApp(AppRoot);
 app.use(PrimeVue, {
   theme: {
+    preset: Lara,
     options: {
       darkModeSelector: '.p-dark'
     }
