4a984fe39717b1c204edeb439f1ccc5efef32550
Dmitry <ziodmitry+gh@gmail.com>
2025-07-12 22:10:07 +0200

    Use PrimeVue Image preview for session photo (#58)

--- visualizer/src/components/SessionStats.ts
+++ visualizer/src/components/SessionStats.ts
@@ -1,16 +1,25 @@
 import { defineComponent } from 'vue';
+import Image from 'primevue/image';
 import TraceVisualizer from './RawTraceVisualizer';
 
 export default defineComponent({
   name: 'SessionStats',
-  components: { TraceVisualizer },
+  components: { TraceVisualizer, Image },
   props: {
-    shots: { type: Array, required: true }
+    shots: { type: Array, required: true },
+    photo: { type: String, default: '' }
   },
   template: `
     <div class="card session-stats">
       <h3>Session Stats</h3>
       <p class="stat-summary">Total shots: {{ shots.length }}</p>
+      <Image
+        v-if="photo"
+        :src="photo"
+        alt="session photo"
+        preview
+        image-class="session-photo"
+      />
       <TraceVisualizer :shots="shots" title="Raw trace" />
     </div>
   `
